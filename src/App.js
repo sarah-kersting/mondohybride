df5d53aaaf30f4da46873eaec47129245477d921
Jeremy Colin <j.colin@searchmetrics.com>
2017-12-14 12:05:19 +0100

    project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Schweinerei verbindungs</h1>
+        </header>
+        <p>Was ist einein Schweinerei</p>
+      </div>
+    );
+  }
+}
+
+export default App;
