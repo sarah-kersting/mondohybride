7e88f92b11c4e6941a5f9cb506cf09acbd7e0afa
BerndEickmann <b.eickmann@searchmetrics.com>
2017-12-21 15:01:45 +0100

    Update BioEngineering.js

--- src/cards/BioEngineering.js
+++ src/cards/BioEngineering.js
@@ -26,7 +26,7 @@ const BioEngineering = ({ classes }) => (
       Hybridwesen zu erzeugen. So hätte es noch vor wenigen Jahren wie Science Fiction geklungen,
       aus der DNA eines verstorbenen Hamsters und eines Kanarienvogels einen neuen kleinen Liebling
       für das Töchterlein zu schaffen, der einzigartig ist. Heute sind solche Sachen keine
-      Seltenheit mehr. (Da Hundekatzenschweinereien momentan noch kurzlebig sind, sind sie als
+      Seltenheit mehr. (Da <i>Hundekatzenschweinereien</i> momentan noch kurzlebig sind, sind sie als
       Geschenke für Kinder eher ungeeignet, wenn man sich die aus ihrem plötzlichen Ableben
       entstehenden Dramen vermeiden will. Anm. des Red.) Die Technik, die diesen Durchbruch
       ermöglichte, ist die populär als 'Genschere' bekannte biochemische Methode Crisp/Cas, which
